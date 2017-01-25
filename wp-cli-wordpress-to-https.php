e30d991fcc01c48ec3f6faae582dd0772cb7f691
Madalin Tache <madalin@madalin.eu>
2017-01-25 02:37:50 +0200

    Forgot a backslash, version bump.

--- wp-cli-wordpress-to-https.php
+++ wp-cli-wordpress-to-https.php
@@ -5,7 +5,7 @@ namespace WP_CLI_WordPressToSSL;
  * Plugin Name: WordPress to HTTPS
  * Plugin URI:  https://madalin.eu
  * Description: Quickly move WordPress sites to HTTPS, also updating .htaccess
- * Version:     1.0.5
+ * Version:     1.0.6
  * Author:      Madalin Tache
  * Author URI:  https://madalin.eu
  * Donate link: https://ko-fi.com/A204JA0
@@ -281,7 +281,7 @@ EOF;
 	/**
 	 * Register wp2ssl as a WP CLI command.
 	 */
-	WP_CLI::add_command( 'migrate-to-ssl', array(
+	\WP_CLI::add_command( 'migrate-to-ssl', array(
 			$convert_site_to_ssl,
 			'start_conversion',
 		), $convert_site_to_ssl->get_command_args());
